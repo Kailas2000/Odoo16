4cc1398b972581152ed8af4a6bb7b1a1f73f381d
Kailas2000 <kailaskrishna004@gmail.com>
2023-10-31 16:39:08 +0530

    updated

---
+++ custom/maintenance_request/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Maintenance Request',
+    'version': '16.0.1.0.0',
+    'depends': [
+        'website',
+        'maintenance',
+        'mail'
+    ],
+    'data':[
+        'data/website_maintenance_menu.xml',
+        'data/request_details_email.xml',
+        'view/maintenance_request_template.xml',
+
+    ],
+}
